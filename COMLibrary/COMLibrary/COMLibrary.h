d2c8947dddfaea0e367c47a596dca41b8b1f5d6a
xb446909 <xiong-bin@live.com>
2016-04-18 20:56:13 +0800

    Add solution

---
+++ COMLibrary/COMLibrary/COMLibrary.h
@@ -0,0 +1,27 @@
+// COMLibrary.h : main header file for the COMLibrary DLL
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CCOMLibraryApp
+// See COMLibrary.cpp for the implementation of this class
+//
+
+class CCOMLibraryApp : public CWinApp
+{
+public:
+	CCOMLibraryApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+	DECLARE_MESSAGE_MAP()
+};
